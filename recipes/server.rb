181f0c07d7852b61d921983236b9777298c333cb
premchandb <premchand.bell@gmail.com>
2017-11-01 20:43:54 +0000

    added version

--- recipes/server.rb
+++ recipes/server.rb
@@ -10,3 +10,11 @@ end
 service 'w3svc' do
 action [ :enable, :start ]
 end
+
+file 'c:\inetpub\wwwroot\Default.htm' do
+content "<h1>Hello, world!</h1>
+<h2>PLATFORM: #{node['platform']}</h2>
+<h2>HOSTNAME: #{node['hostname']}</h2>
+<h2>MEMORY: #{node['memory']['total']}</h2>
+<h2>CPU Mhz: #{node['cpu']['0']['mhz']}</h2>"
+end
