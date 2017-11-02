d33d1e3a97636fde6cbfe4303cdff847e24b0316
premchandb <premchand.bell@gmail.com>
2017-11-02 13:49:37 +0000

    Code refactor with template

--- recipes/server.rb
+++ recipes/server.rb
@@ -11,10 +11,6 @@ service 'w3svc' do
 action [ :enable, :start ]
 end
 
-file 'c:\inetpub\wwwroot\Default.htm' do
-content "<h1>Hello, world!</h1>
-<h2>PLATFORM: #{node['platform']}</h2>
-<h2>HOSTNAME: #{node['hostname']}</h2>
-<h2>MEMORY: #{node['memory']['total']}</h2>
-<h2>CPU Mhz: #{node['cpu']['0']['mhz']}</h2>"
+template 'c:\inetpub\wwwroot\Default.htm' do
+  source 'Default.htm.erb'
 end
